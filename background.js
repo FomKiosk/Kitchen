b78329442dd65a4a1638438914cf4d17b7e7f734
Aryan Eimermacher <info@ssoele.be>
2015-12-18 21:35:49 +0100

    The beginning

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    console.log(chrome);
-    chrome.app.window.create('app.html', {
-        'outerBounds': {
-            'width': 1800,
-            'height': 950
-        }
-    });
-});
